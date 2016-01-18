50b7cffd6bc1aad30df06c460c63b3e9ee54c132
Jeffrey <jeffswigert@gmail.com>
2016-01-18 14:42:56 -0500

    rearrange some stuff with python server

--- hivemind/__init__.py
+++ hivemind/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from make_json_app import make_json_app
+from hivemind.make_json_app import make_json_app
 app = Flask(__name__)
-import hivemind.responses
+import hivemind.views
 import hivemind.api_v1
