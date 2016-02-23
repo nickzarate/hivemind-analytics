8651982ba83f0eae9a95cc3c03805b0d5392e39d
Nick <nicholas.zarate@gmail.com>
2016-02-23 17:18:09 -0500

    Fix views.py to access files from client rather than static and templates

--- hivemind/views.py
+++ hivemind/views.py
@@ -1,7 +1,22 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from hivemind import app
+from os import path, pardir
+from os.path import abspath, join, dirname
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def landing():
+  base = abspath(join(dirname(__file__), pardir))
+  assets = join(base, 'hivemind/client')
+  return send_from_directory(assets, 'index.html')
 
+@app.route('/js/<path:path>')
+def send_js(path):
+  base = abspath(join(dirname(__file__), pardir))
+  assets = join(base, 'hivemind/client/js')
+  return send_from_directory(assets, path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+  base = abspath(join(dirname(__file__), pardir))
+  assets = join(base, 'hivemind/client/css')
+  return send_from_directory(assets, path)
